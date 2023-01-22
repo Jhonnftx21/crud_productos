3c0e630144ae6e15dc8b1355bc41c2b076297cdd
Jhonnftx21 <75653989+Jhonnftx21@users.noreply.github.com>
2023-01-22 09:24:23 -0500

    creación crud versión_1

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+       
+        <title>Laravel</title>    
+
+    
+    </head>
+    <body>  
+   
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                    Listado de productos
+                    <a href="" class="btn btn-success btn-sm  float-end"> Nuevo Producto</a>
+                    </div>
+                    <div class="card-body">
+                        ...
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+    </div>
+    
+   
+</html>
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::get('/', function () {
+    return view('products.index');
+    
+});
+
+
+/*capitulo 5 creación de formulario */ 
